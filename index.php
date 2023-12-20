9f3feee85244106720299b2e7e95169d4d43a1e9
baranero <jakub.baran2@gmail.com>
2023-12-20 23:28:17 +0100

    null-safe operator

--- index.php
+++ index.php
@@ -4,13 +4,10 @@ declare(strict_types = 1);
 
 require_once 'Account.php';
 
-$myAccount = new Account('Tom', 200);
-$johnsAccount = new Account('John', 10);
+$myAccount = null;
 
-$myAccount -> balance = 20;
 
-$myAccount -> deposit(50) -> deposit(30);
+
+$myAccount?->deposit(80)->deposit(30);
 
 var_dump($myAccount);
-echo '<br>';
-var_dump($johnsAccount);
