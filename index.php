f4c32ef4e6f4c979dfddb8573daf6e5d95d1bde2
baranero <jakub.baran2@gmail.com>
2023-12-20 13:40:07 +0100

    passing by reference

--- index.php
+++ index.php
@@ -1,16 +1,13 @@
 <?php
-  $name = "Bro OK";
 
-  $price = 4.99;
-  $tax_rate = 5.2;
-  $quantity = 3;
-  
+$myCup = 'empty';
+$myFriendsCup = 'empty';
 
-  $employed = true;
-  $online = true;
+function fillCup(&$cupParam) {
+  $cupParam = 'filled';
+}
 
-  echo "Hello {$name}<br>";
-  echo "Price is \${$price}<br>";
-  echo "Tax rate is: {$tax_rate}%<br>";
-  echo "Online status: {$online}<br>";
-?>
+fillCup($myCup);
+fillCup($myFriendsCup);
+
+echo $myCup;
