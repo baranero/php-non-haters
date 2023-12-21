7068d807c6015ad0602aca44327bfa857b138ab5
baranero <jakub.baran2@gmail.com>
2023-12-21 08:10:07 +0100

    autoloading classes

--- index.php
+++ index.php
@@ -2,8 +2,15 @@
 
 declare(strict_types = 1);
 
-require_once 'App/Account.php';
-require_once 'App/SocialMedia.php';
+// require_once 'App/Account.php';
+// require_once 'App/SocialMedia.php';
+
+spl_autoload_register(function($class) {
+  $formattedClass = str_replace("\\", "/", $class);
+  $path = "{$formattedClass}.php";
+  
+  require_once $path;
+});
 
 use App\{Account, SocialMedia};
 
